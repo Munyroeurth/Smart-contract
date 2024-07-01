be972f0e04e0547678ac293528c790a308978566
Muny <measmunyroeurth@gmail.com>
2024-07-01 13:43:25 +0700

    muny_feathure_smartcontract

---
+++ test/helloword/hello.sol
@@ -0,0 +1,11 @@
+pragma solidity >=0.5.0 <0.6.0;
+
+contract HelloWorld {
+
+string private mss = "Hello Baby mama ";
+function getmss() view public returns (string memory) {
+    return mss;
+    }
+
+
+}
